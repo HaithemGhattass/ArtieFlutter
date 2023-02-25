fda74240ba031541a262ea0148d2ab93116d4c71
HaithemGhattass <haithem.ghattas@esprit.tn>
2023-02-25 14:14:12 +0100

    splash screen

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,125 @@
+import 'package:artie/constants.dart';
+import 'package:artie/screens/components/splash_content.dart';
+import 'package:artie/size_config.dart';
+import 'package:flutter/material.dart';
+import 'splash_content.dart';
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to Artie, Let’s draw!",
+      "image": "assets/images/splash_robot1.png"
+    },
+    {
+      "text": "let your kid improve his drawing skills with Artie",
+      "image": "assets/images/splash_robot2.png"
+    },
+    {
+      "text": "Your child will be able to color too!",
+      "image": "assets/images/splash_robot3.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]['text'],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      splashData.length,
+                      (index) => buildDot(index: index),
+                    ),
+                  ),
+                  Spacer(flex: 3),
+                  DefaultButton(
+                    text: "Continue",
+                    press: () {},
+                  ),
+                  Spacer()
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int? index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key? key,
+    required this.text,
+    required this.press,
+  }) : super(key: key);
+  final String text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: getProportionateScreenHeight(56),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          primary: Colors.white,
+          backgroundColor: kPrimaryColor,
+        ),
+        onPressed: press as void Function()?,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(18),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:artie/screens/components/body.dart';
+import 'package:artie/size_config.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
